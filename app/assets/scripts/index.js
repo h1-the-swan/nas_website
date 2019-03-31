32cb646bd4af58291bbad58d468bfc2103ed7674
hi-the-swan <jason.portenoy@gmail.com>
2019-03-31 09:16:53 -0700

    have jinja2 handle project templating instead of jquery

--- app/assets/scripts/index.js
+++ app/assets/scripts/index.js
@@ -1,11 +1,11 @@
-const projects = require('./projects.json');
+// const projects = require('./projects.json');
 
-projects.forEach(function(item) {
-	var p = $('#project-item-template').clone();
-	p.find('img').attr('src', STATIC_FOLDER_ROOT + '/img/' + item.img).attr('alt', item.img_alt);
-	var $name = $('<a>').attr('href', SCRIPT_ROOT + item.url);
-	$name.append($('<h5>').html(item.name))
-	p.find('.project-name').append($name);
-	p.find('.project-description').append($('<p>').html(item.description));
-	$('#projects').append(p.show());
-});
+// projects.forEach(function(item) {
+// 	var p = $('#project-item-template').clone();
+// 	p.find('img').attr('src', STATIC_FOLDER_ROOT + '/img/' + item.img).attr('alt', item.img_alt);
+// 	var $name = $('<a>').attr('href', SCRIPT_ROOT + item.url);
+// 	$name.append($('<h5>').html(item.name))
+// 	p.find('.project-name').append($name);
+// 	p.find('.project-description').append($('<p>').html(item.description));
+// 	$('#projects').append(p.show());
+// });
--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,36 @@
-from flask import render_template, url_for, request, config
+import os
+from flask import render_template, url_for, request, json, current_app, redirect
 
 from . import bp as main
 
 @main.route('/')
 def index():
-    return render_template('main/index.html')
+    with current_app.open_resource('main/projects.json') as f:
+        projects = json.load(f)
+    print(projects)
+    return render_template('main/index.html', projects=projects)
 
 @main.route('/coauthorship/')
 def coauthorship_vis():
     fname = url_for('static', filename="data/coauthorship/test_coauthorship_graph_combined_max600.json")
     return render_template('main/coauthorship.html', data_fname=fname)
+
+@main.route('/nautilus/')
+def nautilus_vis():
+    # TODO
+    return redirect(url_for('main.index'))
+
+@main.route('/clustervis/')
+def cluster_compare_vis():
+    # TODO
+    return redirect(url_for('main.index'))
+
+@main.route('/extended_bib/')
+def extended_bib():
+    # TODO
+    return redirect(url_for('main.index'))
+
+@main.route('/keywords/')
+def keyword_mapping():
+    # TODO
+    return redirect(url_for('main.index'))
